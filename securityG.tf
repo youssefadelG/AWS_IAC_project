46b2ad033d52726c2c93c03932fbcdeaf0b75c23
youssefadelG <youssefadel99@gmail.com>
2025-01-01 20:46:32 +0200

    [version_001] allowed internet access in bastion secG

--- securityG.tf
+++ securityG.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = ["192.168.3.0/24"]
   }
 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
